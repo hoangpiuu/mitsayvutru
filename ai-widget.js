eeabf96296f2bcab27f1a7f698e6a01db1898c95
hoangpiuu <hoangpiuu2807@gmail.com>
2025-11-16 22:22:43 +0700

    Auto update luc 16-11-2025 10:22

--- ai-widget.js
+++ ai-widget.js
@@ -69,7 +69,7 @@ MỤC TIÊU:
       .ai-widget-button {
         position: fixed;
         bottom: 330px; /* đưa nút lên trên */
-        right: 20px;
+        right: 1px;
         width: 60px;
         height: 60px;
         border-radius: 999px;
